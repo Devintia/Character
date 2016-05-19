faeebbd5372329cb61e9180073845fd99a6dd046
MiniDigger <admin@minidigger.me>
2016-05-19 19:30:57 +0200

    initial commit

---
+++ src/main/java/net/devintia/character/Character.java
@@ -0,0 +1,7 @@
+package net.devintia.character;
+
+/**
+ * Created by Martin on 19.05.2016.
+ */
+public class Character {
+}
---
+++ src/main/java/net/devintia/character/attribute/Attribute.java
@@ -0,0 +1,7 @@
+package net.devintia.character.attribute;
+
+/**
+ * Created by Martin on 19.05.2016.
+ */
+public class Attribute {
+}
